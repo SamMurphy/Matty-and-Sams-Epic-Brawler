4bdeaa0d00872c3ac83ded7eef3c772a310cd802
MattyY2K9 <matthewhardwick30@hotmail.com>
2016-06-23 16:22:36 +0100

    Added Jumping and Gravity

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,27 +5,39 @@ public class PlayerController : MonoBehaviour {
     public CharacterController character;
 
     public float speed = 3.0f;
+    public float jumpHeight = 3.0f;
+    public float gravity = -10.0f;
+
+    private Vector2 moveDirection;
 
 	// Use this for initialization
 	void Start () 
     {
-	
+        moveDirection = new Vector2(0, 0);	
 	}
 	
 	// Update is called once per frame
 	void Update () 
     {
-	
+        character.Move(moveDirection * Time.deltaTime);
+
+        moveDirection.y += gravity * Time.deltaTime;
+        if (character.isGrounded)
+            moveDirection.y = 0;
+
+        moveDirection.x = 0;
 	}
 
     public void Movement(Vector2 direction)
     {
         direction.x *= speed;
-        character.Move(new Vector3(direction.x, direction.y, 0) * Time.deltaTime);
+        moveDirection += direction;
+        Mathf.Clamp(moveDirection.x, -speed, speed);
     }
 
     public void Jump()
     {
-
+        if (character.isGrounded)
+            moveDirection.y = jumpHeight;
     }
 }
